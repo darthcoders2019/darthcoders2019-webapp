7984ea5b6f2664f94b533e45ed5a32fb43fe34b3
Tashley <tashley@ennov.io>
2019-06-15 19:37:42 +0400

    register full functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgFGdLGB2-s7vHz21153rCN9A_NtpKDPs',
+    authDomain: 'darthcoders-243809.firebaseapp.com',
+    databaseURL: 'https://darthcoders-243809.firebaseio.com',
+    projectId: 'darthcoders-243809',
+    storageBucket: 'darthcoders-243809.appspot.com',
+    messagingSenderId: '21431044238'
+  }
 };
