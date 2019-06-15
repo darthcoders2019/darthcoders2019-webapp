974d61526014f4dc64eeefef4a07a95226871468
Brendon <brendon@ennov.io>
2019-06-15 21:48:28 +0400

    add button translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,8 @@ export class AppComponent {
     this.messagingService.receiveMessage();
     this.message = this.messagingService.currentMessage;
   }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
