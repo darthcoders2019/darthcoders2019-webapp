c15fd4da150e17a13078a5286dc353607ae33c80
Brendon <brendon@ennov.io>
2019-06-15 18:46:04 +0400

    Add push notification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCgFGdLGB2-s7vHz21153rCN9A_NtpKDPs',
+    authDomain: 'darthcoders-243809.firebaseapp.com',
+    databaseURL: 'https://darthcoders-243809.firebaseio.com',
+    projectId: 'darthcoders-243809',
+    storageBucket: 'darthcoders-243809.appspot.com',
+    messagingSenderId: '21431044238'
+  }
 };
 
 /*
