4847ae6bcd4b0bc10e19524494df884f2db4f830
Annaelle <annagenev@gmail.com>
2019-06-15 15:49:50 +0400

    Update Landing page

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -11,5 +11,18 @@ export class LandingpageComponent implements OnInit {
 
   ngOnInit() {
   }
+  isShownStep1: boolean = true;
+  isShownStep2: boolean = false ; // hidden by default
+  
+
+  speechBtn() {
+    this.isShownStep1 = false;
+    this.isShownStep2 = ! this.isShownStep2;
+  }
+
+  textBtn() {
+    this.isShownStep1 = false;
+    this.isShownStep2 = ! this.isShownStep2;
+  }
 
 }
