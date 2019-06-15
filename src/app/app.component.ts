7356fd0d3deb007289d9ff74a10cd6a826726049
Tashley <tashley@ennov.io>
2019-06-15 13:38:37 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'darthcoders2019-webapp';
+}
