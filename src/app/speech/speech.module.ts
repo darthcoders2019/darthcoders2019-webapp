33a38bd9031711158de08a7debc34ba8ce5e1b55
Tashley <tashley@ennov.io>
2019-06-15 16:51:50 +0400

    register form

--- src/app/speech/speech.module.ts
+++ src/app/speech/speech.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SpeechRoutingModule } from './speech-routing.module';
 import { RegisterComponent } from './register/register.component';
@@ -9,6 +10,7 @@ import { LoginComponent } from './login/login.component';
   declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
     SpeechRoutingModule
   ]
 })
--- src/app/text/text-routing.module.ts
+++ src/app/text/text-routing.module.ts
@@ -4,23 +4,25 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-    {
-        path: 'text/login',
-        component: LoginComponent
-      },
-      {
-        path: 'text/register',
-        component: RegisterComponent
-      },
-      {
-        path: '',
-        redirectTo: 'text/login',
-        pathMatch: 'full'
-      }
+  {
+    path: 'text/login',
+    component: LoginComponent
+  },
+  {
+    path: 'text/register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: 'text/login',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class TextRoutingModule { }
