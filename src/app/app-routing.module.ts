ccde0d09b8d26e235b2deee5cd26d26089e1f80c
Annaelle <annagenev@gmail.com>
2019-06-15 15:31:04 +0400

    Set up Landing page + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingpageComponent} from './landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: 'homepage', component: HomepageComponent},
+/*   {path: 'homepage', component: HomepageComponent},
+  {path: 'register', component: RegisterComponent}, */
+  {path: 'home', component: LandingpageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
